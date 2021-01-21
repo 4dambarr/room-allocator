b4c6e485a9fbeaa486f34b4dafa16c7cea97762c
4dambarr <ab2474@york.ac.uk>
2021-01-21 20:16:23 +0000

    Formatting

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { useState } from 'react';
-import { Container, Row, Table } from 'react-bootstrap';
 import AllocateRooms from './AllocateRooms';
+import AllocatedRooms from './AllocatedRooms'
 import './App.css';
 import SetUpPage from './SetupPage';
 
 function App() {
   const [state, setState] = useState('set-up')
+  const [choices, setChoices] = useState()
   const [allocations, setAllocations] = useState(null)
 
   function onAllocateRooms(choices) {
     var result = AllocateRooms(choices)
     setAllocations(result)
+    setChoices(choices)
     setState('allocated')
   }
 
@@ -19,39 +21,13 @@ function App() {
       case 'set-up':
         return <SetUpPage allocateRooms={onAllocateRooms} />
       case 'allocated':
-        console.log(allocations)
         return (
-          <Container>
-            <Row>
-              <div className="Title">
-                <h1>Results</h1>
-              </div>
-            </Row>
-            <Row>
-              <div className='Results-Page'>
-                <div className='Results-Holder'>
-                  <Table className="Table" striped bordered hover>
-                    <thead>
-                      <tr>
-                        <td>Person</td>
-                        <td>Room</td>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {allocations.map(person => {
-                        return (
-                          <tr>
-                            <td>{person.name}</td>
-                            <td>{person.room}</td>
-                          </tr>
-                        )
-                      })}
-                    </tbody>
-                  </Table>
-                </div>
-              </div>
-            </Row>
-          </Container>
+          <AllocatedRooms 
+            allocations={allocations}
+            choices={choices}
+            reallocateRooms={onAllocateRooms}
+            setState={setState}
+          />
         )
       default:
         return <p>Somethings gone wrong</p>
